0a1dbaf3b64fd445666950cf57d0b3969c3ae903
Fagun <mdabuzaforfagun@gmail.com>
2020-11-13 00:46:29 +0600

    Add fluent validator and mediator

--- Gateway/Gateway.API/Startup.cs
+++ Gateway/Gateway.API/Startup.cs
@@ -1,16 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FluentValidation.AspNetCore;
+using Gateway.Domain;
+using MediatR;
 
 namespace Gateway.API
 {
@@ -27,11 +23,13 @@ namespace Gateway.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<DomainConfiguration>());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Gateway.API", Version = "v1" });
             });
+
+            services.AddMediatR(typeof(DomainConfiguration));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Gateway/Gateway.Domain/DomainConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gateway.Domain
+{
+    public class DomainConfiguration
+    {
+    }
+}
