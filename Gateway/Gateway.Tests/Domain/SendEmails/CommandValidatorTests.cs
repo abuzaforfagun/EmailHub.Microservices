bd362d356091d77f5ddd95c4d1c2c2171879bd17
Fagun <mdabuzaforfagun@gmail.com>
2020-11-13 22:21:45 +0600

    Fix test cases

--- Gateway/Gateway.Tests/Domain/SendEmails/CommandValidatorTests.cs
+++ Gateway/Gateway.Tests/Domain/SendEmails/CommandValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using EmailProcessor.Contracts;
 using Gateway.Domain.Features.SendEmail;
 using Xunit;
 
@@ -11,7 +12,7 @@ namespace Gateway.Tests.Domain.SendEmails
         {
             var validator = new SendEmail.CommandValidator();
 
-            var result = validator.Validate(new SendEmail.Command {Content = "FYI", ToEmail = "hr@reciver.com"});
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", ReciverEmail = "hr@reciver.com"});
 
             Assert.Equal(1, result.Errors.Count);
             Assert.Equal("SenderEmail", result.Errors.First().PropertyName);
@@ -22,29 +23,29 @@ namespace Gateway.Tests.Domain.SendEmails
         {
             var validator = new SendEmail.CommandValidator();
 
-            var result = validator.Validate(new SendEmail.Command {Content = "FYI", ToEmail = "hr@reciver.com", SenderEmail = "notaemail"});
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", ReciverEmail = "hr@reciver.com", SenderEmail = "notaemail"});
 
             Assert.Equal(1, result.Errors.Count);
             Assert.Equal("SenderEmail", result.Errors.First().PropertyName);
         }
 
         [Fact]
-        public void SendEmail_CommandValidator_Should_Validate_ToEmail_IsNot_Email()
+        public void SendEmail_CommandValidator_Should_Validate_ReciverEmail_IsNot_Email()
         {
             var validator = new SendEmail.CommandValidator();
 
-            var result = validator.Validate(new SendEmail.Command {Content = "FYI", ToEmail = "notaemail", SenderEmail = "h1@sender.com"});
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", ReciverEmail = "notaemail", SenderEmail = "h1@sender.com"});
 
             Assert.Equal(1, result.Errors.Count);
             Assert.Equal("ToEmail", result.Errors.First().PropertyName);
         }
 
         [Fact]
-        public void SendEmail_CommandValidator_Should_Validate_ToEmail_IsNot_Empty()
+        public void SendEmail_CommandValidator_Should_Validate_ReciverEmail_IsNot_Empty()
         {
             var validator = new SendEmail.CommandValidator();
 
-            var result = validator.Validate(new SendEmail.Command {Content = "FYI", SenderEmail = "hr@sender.com"});
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", SenderEmail = "hr@sender.com"});
 
             Assert.Equal(1, result.Errors.Count);
             Assert.Equal("ToEmail", result.Errors.First().PropertyName);
@@ -55,7 +56,7 @@ namespace Gateway.Tests.Domain.SendEmails
         {
             var validator = new SendEmail.CommandValidator();
 
-            var result = validator.Validate(new SendEmail.Command {Content = "", SenderEmail = "hr@sender.com", ToEmail = "to@reciver.com"});
+            var result = validator.Validate(new SendEmailCommand {Content = "", SenderEmail = "hr@sender.com", ReciverEmail = "to@reciver.com"});
 
             Assert.Equal(1, result.Errors.Count);
             Assert.Equal("Content", result.Errors.First().PropertyName);
