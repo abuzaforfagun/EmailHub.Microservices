f2b24aed0d555b28aafa5e4c4f37ba0bc36275b1
Fagun <mdabuzaforfagun@gmail.com>
2020-11-15 00:02:28 +0600

    Add DistributedConsumer tests

---
+++ Common/Tests/Communication.Tests/DistributedConsumerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Communication.Tests
+{
+    public class DistributedConsumerTests
+    {
+        [Fact]
+        public async Task Process_Should_Call_Mediator_Send()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<object>>();
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            var mediatorMock = new Mock<IMediator>();
+
+            mediatorMock.Setup(m => m.Send(It.IsAny<object>(), It.IsAny<CancellationToken>()));
+            serviceProviderMock
+                .Setup(x => x.GetService(typeof(IMediator)))
+                .Returns(mediatorMock.Object);
+
+            var consumer = new DistributedConsumer(loggerMock.Object, It.IsAny<ServiceBusConfiguration>(),
+                serviceProviderMock.Object);
+
+            // Act
+            await consumer.Process("abc", serviceProviderMock.Object);
+
+            // Assert
+            mediatorMock.Verify(m => m.Send(It.IsAny<object>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
