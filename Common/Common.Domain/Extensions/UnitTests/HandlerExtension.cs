cb0c5609ef9313b538d882520788922ca577bd2b
Fagun <mdabuzaforfagun@gmail.com>
2020-11-14 23:14:23 +0600

    Add test for SendEmail handler

---
+++ Common/Common.Domain/Extensions/UnitTests/HandlerExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Common.Domain.Extensions.UnitTests
+{
+    public static class HandlerExtension
+    {
+        public static async Task<object> Handle<T>(this IRequestHandler<T> handler, T request, CancellationToken cancellationToken = default) where T : IRequest
+        {
+            var castedHandler = (IRequestHandler<T, Unit>)handler;
+            return await castedHandler.Handle(request, cancellationToken);
+        }
+
+        public static async Task<TResponse> Handle<T, TResponse>(this IRequestHandler<T, TResponse> handler, T request, CancellationToken cancellationToken = default) where T : IRequest<TResponse>
+        {
+            var castedHandler = handler;
+            return await castedHandler.Handle(request, cancellationToken);
+        }
+    }
+}
---
+++ Gateway/Gateway.Tests/Domain/SendEmails/HandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Common.Domain.Extensions.UnitTests;
+using Communication;
+using EmailProcessor.Contracts;
+using Gateway.Domain.Features.SendEmail;
+using Logger.Contracts;
+using Moq;
+using Xunit;
+
+namespace Gateway.Tests.Domain.SendEmails
+{
+    public class HandlerTests
+    {
+        [Fact]
+        public async Task Handler_Should_Send_Messages_To_DistributedSender()
+        {
+            var distributedSenderMock = new Mock<IDistributedSender>();
+            distributedSenderMock.Setup(m => m.SendMessageAsync(It.IsAny<object>(), It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(m => m.Map<AddLogCommand>(It.IsAny<SendEmailCommand>()))
+                .Returns(new AddLogCommand());
+
+            var communicationConfigurationMock = new Mock<ICommunicationConfigurationProvider>();
+            communicationConfigurationMock.Setup(m => m.GetQueueName(It.IsAny<object>()))
+                .Returns("send-email");
+
+            var handler = new SendEmail.CommandHandler(distributedSenderMock.Object, communicationConfigurationMock.Object, mapperMock.Object);
+
+            await handler.Handle(new SendEmailCommand(), default);
+
+            distributedSenderMock.Verify(d => d.SendMessageAsync(It.IsAny<object>(), It.IsAny<string>()), Times.Exactly(2));
+        }
+    }
+}
