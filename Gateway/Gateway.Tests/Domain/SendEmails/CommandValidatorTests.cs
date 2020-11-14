350b3d1a8dff15d61010da40c9a39093f3a554bc
Fagun <mdabuzaforfagun@gmail.com>
2020-11-14 22:37:23 +0600

    Add and update test cases for SendEmails CommandValidator

--- Gateway/Gateway.Tests/Domain/SendEmails/CommandValidatorTests.cs
+++ Gateway/Gateway.Tests/Domain/SendEmails/CommandValidatorTests.cs
@@ -12,7 +12,7 @@ namespace Gateway.Tests.Domain.SendEmails
         {
             var validator = new SendEmail.CommandValidator();
 
-            var result = validator.Validate(new SendEmailCommand {Content = "FYI", ReciverEmail = "hr@reciver.com"});
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", ReciverEmail = "hr@reciver.com", ReciverName = "Reciver", Subject = "Subject", SenderName = "Sender"});
 
             Assert.Equal(1, result.Errors.Count);
             Assert.Equal("SenderEmail", result.Errors.First().PropertyName);
@@ -23,7 +23,7 @@ namespace Gateway.Tests.Domain.SendEmails
         {
             var validator = new SendEmail.CommandValidator();
 
-            var result = validator.Validate(new SendEmailCommand {Content = "FYI", ReciverEmail = "hr@reciver.com", SenderEmail = "notaemail"});
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", ReciverEmail = "hr@reciver.com", SenderEmail = "notaemail", ReciverName = "Reciver", Subject = "Subject", SenderName = "Sender"});
 
             Assert.Equal(1, result.Errors.Count);
             Assert.Equal("SenderEmail", result.Errors.First().PropertyName);
@@ -34,10 +34,10 @@ namespace Gateway.Tests.Domain.SendEmails
         {
             var validator = new SendEmail.CommandValidator();
 
-            var result = validator.Validate(new SendEmailCommand {Content = "FYI", ReciverEmail = "notaemail", SenderEmail = "h1@sender.com"});
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", ReciverEmail = "notaemail", SenderEmail = "h1@sender.com", ReciverName = "Reciver", Subject = "Subject", SenderName = "Sender"});
 
             Assert.Equal(1, result.Errors.Count);
-            Assert.Equal("ToEmail", result.Errors.First().PropertyName);
+            Assert.Equal("ReciverEmail", result.Errors.First().PropertyName);
         }
 
         [Fact]
@@ -45,10 +45,10 @@ namespace Gateway.Tests.Domain.SendEmails
         {
             var validator = new SendEmail.CommandValidator();
 
-            var result = validator.Validate(new SendEmailCommand {Content = "FYI", SenderEmail = "hr@sender.com"});
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", SenderEmail = "hr@sender.com", ReciverName = "Reciver", Subject = "Subject", SenderName = "Sender"});
 
             Assert.Equal(1, result.Errors.Count);
-            Assert.Equal("ToEmail", result.Errors.First().PropertyName);
+            Assert.Equal("ReciverEmail", result.Errors.First().PropertyName);
         }
 
         [Fact]
@@ -56,10 +56,43 @@ namespace Gateway.Tests.Domain.SendEmails
         {
             var validator = new SendEmail.CommandValidator();
 
-            var result = validator.Validate(new SendEmailCommand {Content = "", SenderEmail = "hr@sender.com", ReciverEmail = "to@reciver.com"});
+            var result = validator.Validate(new SendEmailCommand {Content = "", SenderEmail = "hr@sender.com", ReciverEmail = "to@reciver.com", ReciverName = "Reciver", Subject = "Subject", SenderName = "Sender"});
 
             Assert.Equal(1, result.Errors.Count);
             Assert.Equal("Content", result.Errors.First().PropertyName);
+        }
+
+        [Fact]
+        public void SendEmail_CommandValidator_Should_Validate_Subject_IsNot_Empty()
+        {
+            var validator = new SendEmail.CommandValidator();
+
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", SenderEmail = "hr@sender.com", ReciverEmail = "to@reciver.com", ReciverName = "Reciver", SenderName = "Sender"});
+
+            Assert.Equal(1, result.Errors.Count);
+            Assert.Equal("Subject", result.Errors.First().PropertyName);
+        }
+
+        [Fact]
+        public void SendEmail_CommandValidator_Should_Validate_ReciverName_IsNot_Empty()
+        {
+            var validator = new SendEmail.CommandValidator();
+
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", SenderEmail = "hr@sender.com", ReciverEmail = "to@reciver.com", Subject = "Greetings", SenderName = "Sender"});
+
+            Assert.Equal(1, result.Errors.Count);
+            Assert.Equal("ReciverName", result.Errors.First().PropertyName);
+        }
+
+        [Fact]
+        public void SendEmail_CommandValidator_Should_Validate_SenderName_IsNot_Empty()
+        {
+            var validator = new SendEmail.CommandValidator();
+
+            var result = validator.Validate(new SendEmailCommand {Content = "FYI", SenderEmail = "hr@sender.com", ReciverEmail = "to@reciver.com", Subject = "Greetings", ReciverName = "Sender"});
+
+            Assert.Equal(1, result.Errors.Count);
+            Assert.Equal("SenderName", result.Errors.First().PropertyName);
         }
     }
 }
